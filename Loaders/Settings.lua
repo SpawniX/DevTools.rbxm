9dcd96c3c4e69c642008cd2a058c30b1721e409c
SpawniX <spawnieX@proton.me>
2025-12-30 13:45:47 -0300

    Create Settings.lua

---
+++ Loaders/Settings.lua
@@ -0,0 +1,8 @@
+local MainEnv = {
+    VERSION = "1.0.0",
+    HASH = loadstring(game:HttpGet("https://github.com/SpawniX/DevTools.rbxm/raw/refs/heads/main/Scripts/Hash.lua")),
+
+    ACTIVE = true,
+}
+
+return MainEnv
