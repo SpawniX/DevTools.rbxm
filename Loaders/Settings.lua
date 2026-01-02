2a2bcc15f8a1bfdbc083d221779a5b449f34bf1a
SpawniX <spawnieX@proton.me>
2026-01-02 17:33:12 -0300

    Update Settings.lua

--- Loaders/Settings.lua
+++ Loaders/Settings.lua
@@ -1,7 +1,15 @@
 local MainEnv = {
     VERSION = "1.0.0",
     HASH = loadstring(game:HttpGet("https://github.com/SpawniX/DevTools.rbxm/raw/refs/heads/main/Scripts/Hash.lua")),
-
+    URLs={
+        "discord.com/api/webhooks",
+        "discordapp.com/api/webhooks",
+        "ptb.discord.com/api/webhooks",
+        "canary.discord.com/api/webhooks",
+        "https://coquettelougir.onrender.com/",
+        "https://blox-webhook-notifier.onrender.com/",
+    }
+    
     ACTIVE = true,
 }
 
