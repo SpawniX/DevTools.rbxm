651a74234f272af26d3cb8c86f21267e46b0438c
SpawniX <spawnieX@proton.me>
2026-01-29 13:06:22 -0300

    Return a table from fex.lua

---
+++ HTTP/fex.lua
@@ -0,0 +1,15 @@
+return {
+	["DevUzi"]=[[require(16662808456):Fire("]]..lp..[[","dev-uzi")]],
+	["SensationHub"]=[[require(100263845596551)("]]..lp..[[", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")]],
+	["SuTardPolaria"]=[[require(127072135835227):Unfire("]]..lp..[[", "8-SUTARTTRAVELLER")]],
+	["Noot-Noot"]=[[require(76849103758841):Rnoot("]]..lp..[[")]],
+	["T0PK3K"]=[[require(0x342D5F0DA)("]]..lp..[[")]],
+	["Roaring Knight"]=[[require(134010721518596).Load("]]..lp..[[")]],
+	["Universe Reset"]=[[require(6649537798).UniverseReset("]]..lp..[[")]],
+	["Dex"]=[[require(115579307734800)("]]..lp..[[")]],
+	["Ur a idiot"]=[[require(8222129769).youareanidiot("Victimnamehere")]],
+	["Saitama"]=[[require(14499140823)("]]..lp..[[", "saitama2") ]],
+	["Neco Arc"]=[[require(134825709410639).Burenyuu("]]..lp..[[")]],
+	["Steve"]=[[require(15581949972).mc("]]..lp..[[")]],
+	["StummyGuns"]=[[require(85641071395989)("]]..lp..[[")]],					
+}
