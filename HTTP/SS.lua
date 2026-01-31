f4f43d3f41ab06d71b880b427be0713d15d18cf0
SpawniX <spawnieX@proton.me>
2026-01-31 17:01:16 -0300

    Create SS.lua

---
+++ HTTP/SS.lua
@@ -0,0 +1,28 @@
+local lp = game.Players.LocalPlayer.Name
+local nig = {
+
+	["DevUzi"]=[[require(16662808456):Fire("]]..lp..[[","dev-uzi")]],
+	["SensationHub"]=[[require(100263845596551)("]]..lp..[[", ColorSequence.new(Color3.fromRGB(71, 148, 253), Color3.fromRGB(71, 253, 160)), "Standard")]],
+	["SuTardPolaria"]=[[require(127072135835227):Unfire("]]..lp..[[", "8-SUTARTTRAVELLER")]],
+	["Noot-Noot"]=[[require(76849103758841):Rnoot("]]..lp..[[")]],
+	["T0PK3K"]=[[require(0x342D5F0DA)("]]..lp..[[")]],
+	["Roaring Knight"]=[[require(134010721518596).Load("]]..lp..[[")]],
+	["Universe Reset"]=[[require(6649537798).UniverseReset("]]..lp..[[")]],
+	["Dex"]=[[require(115579307734800)("]]..lp..[[")]],
+	["Ur a idiot"]=[[require(8222129769).youareanidiot("Victimnamehere")]],
+	["Saitama"]=[[require(14499140823)("]]..lp..[[", "saitama2")]],
+	["Neco Arc"]=[[require(134825709410639).Burenyuu("]]..lp..[[")]],
+	["Steve"]=[[require(15581949972).mc("]]..lp..[[")]],
+	["StummyGuns"]=[[require(85641071395989)("]]..lp..[[")]],	
+	["Gojo V3"]=[[require(77989093857005)("]]..lp..[[")]],
+	["Nix Hand"]=[[require(5813695241).load("]]..lp..[[")]],
+	["New Exser"]=[[require(102321495237505):pls("]]..lp..[[")]],
+	["Pootis V2"]=[[require(75291615834083)("]]..lp..[[")]],
+	["Sheldoni"]=[[require(15629007042)(']]..lp..[[')]],
+	["Wither Storm Panel"]=[[require(110406544116193).WitherStormPanel(]]..lp..[[)]],
+	["Spooky2Blue"]=[[require(132099457182422):blue2spooky(]]..lp..[[)]],
+	["Rocky2Admin"]=[[require(16662837893).AddRocky(]]..lp..[[)]],
+	["Stummy Guns"]=[[require(6677765693)(]]..lp..[[, "welovedooter")]],
+	
+}
+return nig
